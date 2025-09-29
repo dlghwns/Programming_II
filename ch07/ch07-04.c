07351b19f2562cbf8ad28ddd6d00967ec2c8f9e3
dlghwns <dlghw@LEEHOJUN>
2025-09-29 20:30:11 +0900

    07-11

---
+++ ch07/ch07-04.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int exercise0704();
+
+
+int main()
+{
+	exercise0704();
+	return 0;
+}
+
+int exercise0704()
+{
+	int amount[5] = { 1, 1, 5 };		// 1, 1, 5, 0, 0으로 초기화
+	int price[5] = { 0 };				// 배열 전체를 0으로 초기화
+	int i;
+
+	printf("amount = ");
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", amount[i]);
+	}
+	printf("\n");
+
+	printf("price  = ");
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", price[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
