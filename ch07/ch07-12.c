cc7345da9dba6e0498da2678495a9295f0108132
dlghwns <dlghw@LEEHOJUN>
2025-09-29 20:31:20 +0900

    15

---
+++ ch07/ch07-12.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define ROW 3
+#define COL 2
+int exercise0712();
+
+
+int main()
+{
+	exercise0712();
+	return 0;
+}
+
+int exercise0712()
+{
+	int data[ROW][COL];
+	int i, j, k;
+
+	for (i = 0, k = 0; i < ROW; i++)		// 행 인덱스를 증가시킨다.
+	{
+		for (j = 0; j < COL; j++)			// 열 인덱스를 증가시킨다.
+		{
+			data[i][j] = ++k;				// 배열의 원소에 0부터 1씩 커지는 값을 저장한다.
+		}
+	}
+
+	for (i = 0; i < ROW; i++)
+	{
+		for (j = 0; j < COL; j++)
+		{
+			printf("%3d ", data[i][j]);
+		}
+		printf("\n");						// 행마다 줄바꿈 문자를 출력한다.
+	}
+
+	printf("sizeof(data) = %d\n", sizeof(data));
+	printf("sizeof(data[0]) = %d\n", sizeof(data[0]));
+	printf("sizeof(data[0][0]) = %d\n", sizeof(data[0][0]));
+
+	return 0;
+}
---
+++ ch07/ch07-15.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#define SIZE 7
+int exercise0715();
+void copy_array(int source[], int target[], int size);
+void print_array(int arr[], int size);
+
+
+int main()
+{
+	exercise0715();
+	return 0;
+}
+
+int exercise0715()
+{
+	int x[SIZE] = { 10, 20, 30, 40, 50 };
+	int y[SIZE] = { 0 };
+
+	printf("x = ");
+	print_array(x, SIZE);
+	copy_array(x, y, 5);
+	printf("y = ");
+	print_array(y, SIZE);
+
+	return 0;
+}
+
+void copy_array(int source[], int target[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		target[i] = source[i];			// 배열의 원소를 복사한다.
+	}
+}
+
+void print_array(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
